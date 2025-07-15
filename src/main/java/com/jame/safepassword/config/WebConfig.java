264be9e22cd63439d8578695de5607bdaa2b4694
jame-dev13 <jam.dev013@gmail.com>
2025-07-15 12:07:08 -0600

    change adsense

--- src/main/java/com/jame/safepassword/config/WebConfig.java
+++ src/main/java/com/jame/safepassword/config/WebConfig.java
@@ -2,11 +2,8 @@ package com.jame.safepassword.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,10 +19,5 @@ public class WebConfig {
                             .requestMatchers("/", "/tests", "/css/**", "/js/**", "/img/**").permitAll())
                 .csrf(csrf -> csrf.disable());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService testUser(){
-        return new InMemoryUserDetailsManager();
     }
 }
