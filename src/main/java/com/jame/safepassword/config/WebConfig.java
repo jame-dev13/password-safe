56f365f74a0762194bd84031f10a47220d52ba0c
jame-dev13 <jam.dev013@gmail.com>
2025-07-17 14:47:27 -0600

    Security config changed

--- src/main/java/com/jame/safepassword/config/WebConfig.java
+++ src/main/java/com/jame/safepassword/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -17,30 +18,29 @@ public class WebConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())
                 .cors(cors -> corsConfigurationSource())
-            .authorizeHttpRequests(request ->
-                    request.requestMatchers("/api/v1/**")
-                            .permitAll()
-                            .requestMatchers("/", "/tests", "/css/**", "/js/**", "/img/**").permitAll());
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/api/v1/**").permitAll()
+                                .requestMatchers("/", "/tests", "/css/**", "/js/**", "/img/**").permitAll()
+                                .anyRequest().authenticated()
+                );
         return http.build();
     }
 
-    public void corsConfigurationSource(){
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of(
-                "Origin",
-                "Content-Type",
-                "Accept",
-                "Authorization",
-                "Access-Control-Request-Method",
-                "Access-Control-Request-Headers",
-                "Access-Control-Allow-Origin"
-        ));
-        corsConfiguration.setAllowedMethods(List.of("GET", "OPTIONS"));
         corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of(
+                "Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        corsConfiguration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
